2f02cd58416156464cc3e2057335d7fe198301cc
PrabeshBimali <prabeshlord111@gmail.com>
2026-01-20 09:52:27 +0545

    Added Rooms table in database

---
+++ migrations/1768880113434_create-rooms-table.sql
@@ -0,0 +1,11 @@
+-- Up Migration
+CREATE TABLE IF NOT EXISTS public.rooms
+(
+    id uuid DEFAULT gen_random_uuid(),
+    created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    last_message_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    CONSTRAINT pk_rooms PRIMARY KEY (id)
+);
+
+-- Down Migration
+DROP TABLE public.rooms
